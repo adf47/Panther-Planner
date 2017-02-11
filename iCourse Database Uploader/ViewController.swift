98491141b0de056fba29ef9f9953294ecf500890
Antonino Febbraro <adf47@pitt.edu>
2017-02-11 17:37:23 -0500

    Finished parsing file in swift for database upload
    Now need to write data to the CloudKit Database

--- iCourse Database Uploader/ViewController.swift
+++ iCourse Database Uploader/ViewController.swift
@@ -9,6 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var file = ""
+    var x = 0
+    
+    var classNum = ""
+    var term = ""
+    var major = ""
+    var courseNum = ""
+    var classTitle = ""
+    var credits = ""
+    var satGenEdReq = ""
+    var DayTimeClassRoomBuilding = ""
+    var prof = ""
+    var descrip = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +36,55 @@ class ViewController: NSViewController {
         }
     }
 
-
+    
+    
+    @IBAction func UpdateDatabase(_ sender: Any) {
+        
+        if let fileURL = Bundle.main.url(forResource:"Courses", withExtension: ".txt")
+        {
+            do {
+                let contents = try String(contentsOf: fileURL, encoding: String.Encoding.utf8)
+                //print(contents)
+                file = contents
+            } catch {
+                print("Error: \(error)")
+            }
+        } else {
+            print("No such file URL.")
+        }
+        
+        UploadFile()
+        
+    }
+    
+    func UploadFile(){
+        
+        print("\n Uploading to database...")
+        let myStringArr = file.components(separatedBy: "\n")
+        
+        while(x<myStringArr.count){
+            
+            //print(myStringArr[8])
+            
+            if(x == myStringArr.count - 1){
+                break
+            }
+            classNum = myStringArr[x]
+            term = myStringArr[x+1]
+            major = myStringArr[x+2]
+            courseNum = myStringArr[x+3]
+            classTitle = myStringArr[x+4]
+            credits = myStringArr[x+5]
+            satGenEdReq = myStringArr[x+6]
+            DayTimeClassRoomBuilding = myStringArr[x+7]
+            prof = myStringArr[x+8]
+            descrip = myStringArr[x+10]
+            
+            x = x + 14
+            //print(prof)
+        }
+    }
+    
+    
 }
 
