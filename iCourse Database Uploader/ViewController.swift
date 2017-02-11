57b2544750c498de8ab0901e8f4c7c47b6decc87
Antonino Febbraro <adf47@pitt.edu>
2017-02-11 16:14:17 -0500

    Uploading Desktop Database Uploader Program

---
+++ iCourse Database Uploader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iCourse Database Uploader
+//
+//  Created by Antonino Febbraro on 2/11/17.
+//  Copyright © 2017 Antonino Febbraro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
