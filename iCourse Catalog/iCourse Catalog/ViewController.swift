ba0fbc0eff91d1b16aef4a7cd120028d5a5b44e0
Antonino Febbraro <adf47@pitt.edu>
2017-02-22 17:55:31 -0500

    Created iOS Project for iCourse Catalog App
    Making progress

---
+++ iCourse Catalog/iCourse Catalog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCourse Catalog
+//
+//  Created by Antonino Febbraro on 2/22/17.
+//  Copyright © 2017 Antonino Febbraro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
