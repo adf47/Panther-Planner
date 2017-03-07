3f647eeb5841587e977d849fed8d4ce9258091a1
Antonino Febbraro <adf47@pitt.edu>
2017-03-07 18:16:36 -0500

    Added border

--- iCourse Catalog/iCourse Catalog/SeeMajorsView.swift
+++ iCourse Catalog/iCourse Catalog/SeeMajorsView.swift
@@ -206,7 +206,8 @@ class SeeMajorsView: UIViewController, CLLocationManagerDelegate, MKMapViewDeleg
         cell.professor.text = self.profArray[indexPath.row]
         
         cell.backgroundColor = UIColor.white // make cell more visible in our example project
-        
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = UIColor.black.cgColor //hexStringToUIColor(hex: "#979797") as? CGColor
         self.buttons.append(cell)
         //print("returning cell!!")
         return cell
